421d4c15082e42355e40a417d45f85a130f5845e
HelloMandu <tjdals6695@gmail.com>
2021-02-04 23:40:00 +0900

    motion project setting, basecomponent 작성

---
+++ motion/src/app.ts
@@ -0,0 +1,12 @@
+import { ImageComponent } from './components/page/item/image.js';
+import { PageComponent }  from './components/page/page.js';
+
+class App{
+    private readonly page: PageComponent;
+    constructor(appRoot: HTMLElement){
+        this.page = new PageComponent();
+        this.page.attachTo(appRoot);
+    }
+}
+
+new App(document.querySelector('.document')! as HTMLElement);
---
+++ motion/src/components/page/item/image.ts
@@ -0,0 +1,19 @@
+import { BaseComponent } from '../../component.js';
+
+export class ImageComponent extends BaseComponent<HTMLElement>{
+    constructor(title: string, url: string){
+        super(`
+            <section class="image">
+                <div class="image__holder">
+                    <img src="" alt="" class="image__thumbnail">
+                </div>
+                <p class="image__title"></p>
+            </section>
+        `);
+        const imageElement = this.element.querySelector('.image__thumbnail')! as HTMLImageElement;
+        imageElement.src = url;
+        imageElement.alt = title;
+        const titleElemnt = this.element.querySelector('.image__title')! as HTMLParagraphElement;
+        titleElemnt.textContent = title;
+    }
+}
